8807307be3a69d56a5d735f7f458109cedcf6418
Spotlight <spotlight@joscomputing.space>
2022-11-08 15:37:26 -0600

    Implement basic configuration
    This will eventually expand over time, so it's implemented very loosely.

---
+++ src/config.rs
@@ -0,0 +1,44 @@
+use once_cell::sync::OnceCell;
+use serde::Deserialize;
+use std::{fs, net::SocketAddr};
+
+#[derive(Debug, Deserialize)]
+pub struct Config {
+    pub service: ServiceConfig,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct ServiceConfig {
+    #[serde(default = "default_bind_address")]
+    pub bind_address: SocketAddr,
+    pub base_domain: String,
+    pub base_identifier: String,
+}
+
+// Per configuration, we should bind to 127.0.0.1:8080 by default.
+fn default_bind_address() -> SocketAddr {
+    "127.0.0.1:8080".parse().unwrap()
+}
+
+static CONFIG: OnceCell<Config> = OnceCell::new();
+
+/// Used to access options within configuration.
+impl Config {
+    /// Retrieves the loaded configuration for this application.
+    pub fn shared() -> &'static Config {
+        CONFIG.get().expect("config was not initialized")
+    }
+
+    /// Retrieves general domain configuration for this application.
+    pub fn service() -> &'static ServiceConfig {
+        &Config::shared().service
+    }
+
+    /// Loads the configuration from the specified path to our shared OnceCell.
+    /// Please do not invoke this manually -- instead, access via shared.
+    pub fn load_from(path: String) -> () {
+        let contents = fs::read_to_string(path).expect("failed to read configuration");
+        let config = toml::from_str(&contents).expect("unable to parse configuration");
+        CONFIG.set(config).expect("unable to set configuration")
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
+mod config;
 mod plist;
 mod routes;
+
+use crate::config::Config;
+use std::env;
 
 #[tokio::main]
 async fn main() {
     println!("Starting up...");
 
-    axum::Server::bind(&"127.0.0.1:8080".parse().unwrap())
+    // TODO: Allow for a flag-based way to specify this config path
+    let args: Vec<String> = env::args().collect();
+    let config_path = match args.get(1) {
+        Some(s) => s.clone(),
+        None => "./config.toml".to_string(),
+    };
+    Config::load_from(config_path);
+
+    axum::Server::bind(&Config::service().bind_address)
         .serve(routes::create_routes().into_make_service())
         .await
         .unwrap();
