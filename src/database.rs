5f51af4e2d42afb6ead2b966d8a25adb839b0054
Spotlight <spotlight@joscomputing.space>
2022-11-11 13:23:39 -0600

    Ensure we can create the database
    This is more of a best-effort attempt than actual assurance, but it assists highly with development.

--- src/database.rs
+++ src/database.rs
@@ -1,8 +1,10 @@
+use crate::config::Config;
 use axum::Extension;
 use r2d2::{Pool, PooledConnection};
 use r2d2_sqlite::SqliteConnectionManager;
-
-use crate::config::Config;
+use std::fs;
+use std::path::Path;
+use std::time::Duration;
 
 #[derive(Clone)]
 pub struct Database {
@@ -13,12 +15,28 @@ pub struct Database {
 impl Database {
     /// Opens the pool against the specified SQLite3 database.
     pub fn open() -> Result<Extension<Database>, r2d2::Error> {
-        let database_path = &Config::storage().database_path;
+        let database_path = Path::new(&Config::storage().database_path);
+        Self::attempt_create_parents(database_path);
         let manager = SqliteConnectionManager::file(database_path);
-        let pool = r2d2::Pool::new(manager)?;
+        let pool = r2d2::Builder::new()
+            .connection_timeout(Duration::from_secs(5))
+            .build(manager)?;
         Ok(Extension(Database {
             internal_pool: pool,
         }))
+    }
+
+    /// Creates parent directories for our SQLite3 database if they do not already exist.
+    fn attempt_create_parents(database_path: &Path) {
+        // Check to see if we need to do anything.
+        if database_path.exists() {
+            return;
+        }
+
+        // Create parent directories, if necessary.
+        let parents = database_path.parent().unwrap();
+        fs::create_dir_all(parents)
+            .expect("should be able to create parent directories for database")
     }
 
     /// Begins a new connection.
