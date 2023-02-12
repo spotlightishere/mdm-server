be60a504e366d45bed8511d37dac3f5717b52284
Spotlight <spotlight@joscomputing.space>
2023-02-11 21:22:29 -0600

    Rename pkcs7 to pkcs7_body
    This conflicts with a crate of the same name.

--- src/certificates/mod.rs
+++ src/certificates/mod.rs
@@ -2,7 +2,7 @@ mod apple_device_ca;
 mod certs;
 mod file;
 mod generator;
-mod pkcs7;
+mod pkcs7_body;
 
 pub use certs::Certificates;
-pub use pkcs7::{Pkcs7Body, Pkcs7Signer};
+pub use pkcs7_body::{Pkcs7Body, Pkcs7Signer};
