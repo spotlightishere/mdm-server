863e607877cb5ff6e485a812f38ae85d80609801
Spotlight <spotlight@joscomputing.space>
2023-06-19 08:25:23 -0500

    Apply TimestamptzSqlite patch via Diesel itself
    This will have to be updated for future `TimestamptzSqlite` usage, of course, but it greatly assists right now.

--- src/database/schema.rs
+++ src/database/schema.rs
@@ -7,9 +7,6 @@ diesel::table! {
         product -> Text,
         serial_number -> Text,
         imei -> Nullable<Text>,
-        // TODO: For now, we need to manually change
-        // `Timestamp` to `TimestamptzSqlite` due to
-        // https://github.com/diesel-rs/diesel/issues/3320
         last_contact -> TimestamptzSqlite,
     }
 }
